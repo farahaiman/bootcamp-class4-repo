18f74adb67baefd179627351751b8bbba9c7088c
farah <farahaiman548@gmail.com>
2020-11-17 13:44:12 +0500

    test2 action file work or not

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
+import {Msg} from './Msg.jsx';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello</h1>
+    <div className={`App ${color ? 'change-color' : ''}`}>
+    {/* <h1>PAKISTAN ZENDABAAD {count} </h1> */}
+    <Msg counter={ count}/>
+    <h3>Color Changer ={ color ? 'green' : ' drak blue'}</h3>
+    <br/>
+    <button onClick={()=> setCount(++count)}>Update Value</button>
+    <br/>
+    <button onClick={()=> setColor(!color)}>Update Color</button>
+    <div> 
+
+      hi code main eror agya :( galt build bna li :(
+        <h2>surge ulr work nhi kr rah </h2>
+        <h2>surge ulr change kr diya phir bhi kam nhi kr rah h masla kya h </h2>
+        <h2>surge ulr error test6</h2>
     </div>
+   
+  </div>
   );
 }
 
---
+++ src/Msg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ export function Msg( props) {
+    return (
+        <h1>PAKISTAN ZENDABAAD { props.counter} </h1>
+    )
+}
+
+export default Msg
